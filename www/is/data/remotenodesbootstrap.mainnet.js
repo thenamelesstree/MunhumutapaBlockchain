26291869da55d3216aedec6141800d7ad2862331
munhumutapaalpha <73066748+munhumutapaalpha@users.noreply.github.com>
2020-10-20 06:34:52 +0200

    Add files via upload

---
+++ www/is/data/remotenodesbootstrap.mainnet.js
@@ -0,0 +1,65 @@
+/******************************************************************************
+ * Copyright © 2013-2016 The Nxt Core Developers.                             *
+ * Copyright © 2016-2019 Jelurida IP B.V.                                     *
+ *                                                                            *
+ * See the LICENSE.txt file at the top-level directory of this distribution   *
+ * for licensing information.                                                 *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement with Jelurida B.V.,*
+ * no part of this software, including this file, may be copied, modified,    *
+ * propagated, or distributed except according to the terms contained in the  *
+ * LICENSE.txt file.                                                          *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+RemoteNodesManager.prototype.REMOTE_NODES_BOOTSTRAP =
+    {
+        "peers":[
+            {
+                "downloadedVolume":0,
+                "address":"82.253.237.216",
+                "inbound":false,
+                "blockchainState":"UP_TO_DATE",
+                "uploadedVolume":0,
+                "services":[
+                    "API",
+                    "CORS"
+                ],
+                "version":"2.2.2",
+                "platform":"Linux arm",
+                "lastUpdated":1,
+                "blacklisted":false,
+                "announcedAddress":"82.253.237.216",
+                "apiPort":22024,
+                "application":"Munhumutapa",
+                "port":22022,
+                "lastConnectAttempt":-,
+                "state":2,
+                "shareAddress":true
+            },
+            {
+                "downloadedVolume":0,
+                "address":"88.151.148.10",
+                "inbound":false,
+                "blockchainState":"UP_TO_DATE",
+                "uploadedVolume":0,
+                "services":[
+                    "API",
+                    "CORS"
+                ],
+                "version":"2.2.2",
+                "platform":"Linux arm",
+                "lastUpdated":1,
+                "blacklisted":false,
+                "announcedAddress":"88.151.148.10",
+                "apiPort":22024,
+                "application":"Munhumutapa",
+                "port":22022,
+                "lastConnectAttempt":,
+                "state":2,
+                "shareAddress":true
+            },
+             ]
+    };
